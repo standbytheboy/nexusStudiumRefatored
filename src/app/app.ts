650d7a481db95ede93f5a65233fcc540661c5990
standbytheboy <moraislcs05@gmail.com>
2025-10-02 16:20:49 -0300

    enhancing geral styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./layout/sidebar/sidebar";
 import { YourContributions } from "./pages/your-contributions/your-contributions";
+import { DashboardComponent } from "./pages/dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, YourContributions],
+  imports: [SidebarComponent, YourContributions, DashboardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
