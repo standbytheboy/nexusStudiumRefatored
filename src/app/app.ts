88ca9affde4fefaa91c1fd1e779f15fc98e1d320
standbytheboy <moraislcs05@gmail.com>
2025-10-04 17:27:10 -0300

    creating article card for courses and more

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./layout/sidebar/sidebar";
 import { YourContributions } from "./pages/your-contributions/your-contributions";
 import { Dashboard } from "./pages/dashboard/dashboard";
+import { Articles } from "./shared/articles/articles";
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, YourContributions, Dashboard],
+  imports: [SidebarComponent, YourContributions, Dashboard, Articles],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
