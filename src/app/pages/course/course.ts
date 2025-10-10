2ba5004ad628eef9ecb3dbc6abdf4bba2fcce217
standbytheboy <moraislcs05@gmail.com>
2025-10-09 21:38:54 -0300

    adding search-bar on course page

--- src/app/pages/course/course.ts
+++ src/app/pages/course/course.ts
@@ -3,10 +3,11 @@ import { Trail } from "../../shared/trail/trail";
 import { CourseHeader } from "../../shared/course-header/course-header";
 import { CoursePlayer } from "../../shared/course-player/course-player";
 import { SidebarComponent } from "../../layout/sidebar/sidebar";
+import { SearchBar } from "../../layout/search-bar/search-bar";
 
 @Component({
   selector: 'app-course',
-  imports: [Trail, CourseHeader, CoursePlayer, SidebarComponent],
+  imports: [Trail, CourseHeader, CoursePlayer, SidebarComponent, SearchBar],
   templateUrl: './course.html',
   styleUrl: './course.css'
 })
