1447c33a0ac39a84f1e2ab9d86fe7876614eae3a
standbytheboy <moraislcs05@gmail.com>
2025-11-24 16:44:45 -0300

    finishing article page

--- src/app/pages/articles/articles.ts
+++ src/app/pages/articles/articles.ts
@@ -3,23 +3,24 @@ import { SearchBar } from "../../layout/search-bar/search-bar";
 import { Navigation } from "../../layout/navigation/navigation";
 import { MainSection, MainInfoProps } from '../../shared/main-section/main-section';
 import { CommonModule } from '@angular/common';
+import { ArticleCard } from "../../shared/article-card/article-card";
 
 @Component({
   selector: 'app-articles',
-  imports: [SearchBar, Navigation, MainSection, CommonModule],
+  imports: [SearchBar, Navigation, MainSection, CommonModule, ArticleCard],
   templateUrl: './articles.html',
   styleUrl: './articles.css',
 })
 export class Articles {
-    public articleInfo: MainInfoProps[] = [
+    public mainArticle: MainInfoProps[] = [
     {
       image: 'https://images.unsplash.com/photo-1750785354752-2c234b875cdd',
-      title: 'Figma: Dicas Essenciais para Iniciantes',
-      subtitle: 'Figma: Dicas Essenciais para Iniciantes',
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       author: 'Ana Clara',
       authorRole: 'Product Designer',
       authorAvatar: 'https://images.unsplash.com/photo-1485846234645-a62644f84728',
-      answerCount: 10,
+      minutesToRead: 10,
     },
   ];
 }
